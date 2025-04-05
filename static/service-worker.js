472e751a3813303080ca55135072a2674cee602a
ecurrier <ecurri3@gmail.com>
2025-04-05 15:23:32 -0500

    Fixed entry files not being mapped to manifest post-build

--- static/service-worker.js
+++
@@ -1,9 +0,0 @@
-const AppNamespaces = ["Global", "Utilities", "Templates", "WebAPI", "Security", "Settings"];
-
-chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-    if (!AppNamespaces.includes(request.category)) {
-        return;
-    }
-
-    chrome.runtime.sendMessage(null, request);
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,40 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), ManifestRewritePlugin()],
+	build: {
+		rollupOptions: {
+			input: {
+				content: resolve(__dirname, 'src/content/index.ts'),
+				background: resolve(__dirname, 'src/background/index.ts')
+			},
+			output: {
+				entryFileNames: '[name].js'
+			}
+		}
+	}
 });
+
+function ManifestRewritePlugin() {
+	return {
+		name: 'manifest-rewrite',
+		apply: 'build',
+		closeBundle() {
+			const manifestPath = path.join('build', 'manifest.json');
+			let manifest = readFileSync(manifestPath, 'utf-8');
+
+			const files = readdirSync('build/app/immutable');
+			const getFile = (match: string) => files.find((f) => f.includes(match)) || '';
+
+			manifest = manifest
+				.replace('__BACKGROUND__', getFile('background') || 'background.js')
+				.replace('__CONTENT__', getFile('content') || 'content.js');
+
+			writeFileSync(manifestPath, manifest);
+		}
+	};
+}
